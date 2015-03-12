bc532cfb5495af9720adae2acb0daeaf6c4c384b
Ksiona <ksyuhl@mail.ru>
2015-03-13 03:59:08 +0400

    technical commit
    Was removed deploy and pre-build folders

--- Tracker-ejb/src/java/ru/shmoylova/tracker/interfaces/dao/BaseEntity.java
+++ Tracker-ejb/src/java/ru/shmoylova/tracker/interfaces/dao/BaseEntity.java
@@ -1,9 +1,8 @@
 package ru.shmoylova.tracker.interfaces.dao;
 
 /**
- * @author Ksiona
- * an empty interface for later use entities in DAO
+ * @author Ksiona an empty interface for later use entities in DAO
  */
 public interface BaseEntity {
-    
+
 }
--- Tracker-ejb/src/java/ru/shmoylova/tracker/util/HibernateUtil.java
+++ Tracker-ejb/src/java/ru/shmoylova/tracker/util/HibernateUtil.java
@@ -7,18 +7,18 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-	private static StandardServiceRegistry serviceRegistry;
-	private static SessionFactory sessionFactory;
+    private static StandardServiceRegistry serviceRegistry;
+    private static SessionFactory sessionFactory;
 
-	 static {
-		Configuration configuration = new Configuration();
-		configuration.configure();
-		serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
-				configuration.getProperties()).build();
-		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-		
-	}
- 
+    static {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+                configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+    }
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
